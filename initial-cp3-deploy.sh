ecf5a1485852193e637afe6795acc6a7ec84d6c2
mirr254 <sammysteppa90@gmail.com>
2018-07-03 22:49:18 +0300

    fix url string in conf file

--- initial-cp3-deploy.sh
+++ initial-cp3-deploy.sh
@@ -46,7 +46,7 @@ function setUpNginx {
         server {
             listen 80;
             location / {
-                proxy_pass http:127.0.0.1:8000/;
+                proxy_pass http://127.0.0.1:8000/;
             }
         }
 ENDOFFILE
